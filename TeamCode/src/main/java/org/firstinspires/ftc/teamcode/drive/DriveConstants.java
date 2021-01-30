262d9cdf087f340526cb5b746e8994198bcf8e00
Jaitra Ajgaonkar <43525435+Jaitra101@users.noreply.github.com>
2021-01-30 15:27:59 -0600

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(25, 0, 1, 12.5);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(22.5, 0, 1, 12.5);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -44,7 +44,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 17; // in
+    public static double TRACK_WIDTH = 16.25; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
